1d3598a5794b3515c5c6b29428da0124f91f5373
Steve Allison <steve@sobrio.tech>
2019-10-29 10:27:29 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "notes-app-upload-landing-zone"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://1xuc066jf3.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_0veobnmmF",
+      APP_CLIENT_ID: "3dkaf2m11oppid61j4hl2atk21",
+      IDENTITY_POOL_ID: "eu-west-2:9e942c5c-d335-4746-9714-81b54e8a018f"
+    }
+  };
